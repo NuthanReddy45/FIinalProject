c176dcc7eff79abfc88303cad0df28ab094b6e2b
NuthanReddy45 <sainuthanreddy264@gmail.com>
2023-05-15 17:21:36 +0530

    cors fix

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 ReactDOM.render(<App />, document.getElementById("root"));

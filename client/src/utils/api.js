8b686c5fc03f9ec934ba8646f3015d5add0ebf6c
NuthanReddy45 <sainuthanreddy264@gmail.com>
2023-05-15 14:43:03 +0530

    deployed backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,8 @@ import store from "../store";
 import { LOGOUT } from "../actions/types";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://elabour.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
